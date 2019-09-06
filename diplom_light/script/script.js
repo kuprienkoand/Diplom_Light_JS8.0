d29ec33442de400f2661bd5f243e17d1c3122e95
Andrey Kuprienko <lupp@ukr.net>
2019-09-07 00:25:16 +0200

    Форма отправки посредством ajax

--- diplom_light/script/script.js
+++ diplom_light/script/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  // popup-call. При клике на Перезвоните мне, вызываться модальное окно
+  // popup-call. При клике на Перезвоните мне, вызывается модальное окно
 
   const popupMod = () => {
 
@@ -31,6 +31,89 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   popupMod();
+
+  // Only number. В поле с телефоном можно вводить только цифры
+
+  const formValidationPhone = () => {
+    let formPhone = document.querySelectorAll('.phone-user');
+
+    formPhone.forEach((item) => {
+      item.addEventListener('input', () => {
+        item.value = item.value.replace(/[^+\d]/gi, '');
+      });
+    });
+  };
+
+  formValidationPhone();
+
+  // Форма отправки посредством ajax
+
+  const sendForm = () => {
+
+    const errorMessage = 'Что-то пошло не так...',
+      loadMessage = 'Загрузка...',
+      successMessage = 'Спасибо! Мы скоро с вами свяжемся';
+
+    const formAll = document.querySelectorAll('form');
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 20px';
+
+
+    formAll.forEach((item) => {
+      item.addEventListener('submit', (event) => {
+        event.preventDefault();
+        item.appendChild(statusMessage);
+        statusMessage.textContent = loadMessage;
+        const formData = new FormData(item);
+        let body = {};
+
+        formData.forEach((val, key) => {
+          body[key] = val;
+        });
+
+        postData(body)
+          .then((response) => {
+            if (response.status !== 200) {
+              throw new Error('status network not 200.');
+            }
+            item.reset();
+            statusMessage.textContent = successMessage;
+            setTimeout(() => {
+              statusMessage.textContent = '';
+            }, 3000);
+          })
+          .catch((error) => {
+            item.reset();
+            statusMessage.textContent = errorMessage;
+            console.error(error);
+            statusMessage.style.cssText = 'font-size: 20px; font-weight: bold; color: red';
+            setInterval(() => {
+              statusMessage.style.opacity ^= 1;
+            }, 1000);
+          });
+
+        let formUserQuest = document.querySelector('.director-form input');
+        if (formUserQuest.value) {
+          document['.director-form input'] = formUserQuest.value;
+          formUserQuest.value = '';
+        }
+      });
+    });
+
+    const postData = (body) => {
+      return fetch('./server.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body),
+        credentials: 'include'
+      });
+    };
+  };
+
+  sendForm();
 
 
 });
