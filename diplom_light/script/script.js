b3280ec00f60102f4b85f3c117a7ba22bc66cabe
Andrey Kuprienko <lupp@ukr.net>
2019-09-08 04:34:04 +0200

    Добавлен popup-consultation

--- diplom_light/script/script.js
+++ diplom_light/script/script.js
@@ -70,6 +70,27 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       }
     });
+
+    // Модальное окно со скидкой popup-consultation
+
+    const popupConsultation = document.querySelector('.popup-consultation'),
+      consultationBtn = document.querySelector('.consultation-btn');
+
+    consultationBtn.addEventListener('click', () => {
+      popupConsultation.style.display = 'block';
+    });
+
+    popupConsultation.addEventListener('click', (event) => {
+      let target = event.target;
+      if (target.classList.contains('popup-close')) {
+        popupConsultation.style.display = 'none';
+      } else {
+        target = target.closest('.popup-content');
+        if (!target) {
+          popupConsultation.style.display = 'none';
+        }
+      }
+    });
   };
 
   popupMod();
