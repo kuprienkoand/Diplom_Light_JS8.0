dc452162bea2e5466e89e0485810a029714228db
Andrey Kuprienko <lupp@ukr.net>
2019-09-08 02:49:15 +0200

    Добавлен Конструктор калькулятор аккордеон

--- diplom_light/script/script.js
+++ diplom_light/script/script.js
@@ -153,5 +153,55 @@ window.addEventListener('DOMContentLoaded', () => {
 
   accordionQuestion();
 
+  // Конструктор калькулятор аккордеон
+
+  const constructorCalc = () => {
+
+    let accordionOne = document.getElementById('accordion'),
+      constructBtn = accordionOne.querySelectorAll('.construct-btn'),
+      panelCollapse = accordionOne.querySelectorAll('.panel-collapse'),
+      panelHeading = accordionOne.querySelectorAll('.panel-heading'),
+      nextBtn = document.querySelectorAll('a');
+
+    nextBtn.forEach((item) => {
+      item.href = "#!";
+    });
+
+    const toggleCalcConst = (index) => {
+      for (let i = 0; i < panelCollapse.length; i++) {
+        if (index === i) {
+          panelCollapse[i].classList.add('in');
+        } else {
+          panelCollapse[i].classList.remove('in');
+        }
+      }
+    };
+
+    accordionOne.addEventListener('click', (event) => {
+      let target = event.target;
+      target = target.closest('.panel-heading');
+      if (target) {
+        panelHeading.forEach((item, i) => {
+          event.preventDefault();
+          if (item === target) {
+            toggleCalcConst(i);
+          }
+        });
+      }
+    });
+
+      for (let i = 0; i < panelHeading.length; i++) {
+      constructBtn[i].addEventListener('click', () => {
+        if (i !== 3) {
+          panelCollapse.forEach((elem) => {
+            elem.classList.remove('in');
+          });
+          panelCollapse[i + 1].classList.add('in');
+        }
+      });
+    }
+  };
+  
+ constructorCalc();
 
 });
