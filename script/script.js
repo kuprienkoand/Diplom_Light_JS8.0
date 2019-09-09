6ab8fbaae2c9aca631cdfbfb13d1dfb2992bb2b1
Andrey Kuprienko <lupp@ukr.net>
2019-09-09 21:31:31 +0200

    Внесены изменения в калькулятор

--- script/script.js
+++ script/script.js
@@ -271,39 +271,69 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const calculator = () => {
 
-    const myonoffswitch = document.getElementById('myonoffswitch'),
-      titleText = document.querySelectorAll('.title-text'),
-      selectBox = document.querySelectorAll('.select-box');
+   const titleText = document.querySelectorAll('.title-text'),
+     selectBox = document.querySelectorAll('.select-box'),
+     constructor = document.querySelector('.constructor'),
+     checkbox = constructor.querySelectorAll('.onoffswitch-checkbox'),
+     calcResult = document.getElementById('calc-result'),
+     formControl = constructor.querySelectorAll('.form-control');
 
-    const changeCell = () => {
-      titleText[1].style.display = 'none';
-      selectBox[2].style.display = 'none';
-      selectBox[3].style.display = 'none';
+   titleText[1].style.display = 'none';
+   selectBox[2].style.display = 'none';
+   selectBox[3].style.display = 'none';
+   checkbox[1].checked = false;
 
-      myonoffswitch.addEventListener('change', () => {
-        if (myonoffswitch.checked) {
-          titleText[1].style.display = 'none';
-          selectBox[2].style.display = 'none';
-          selectBox[3].style.display = 'none';
-        } else {
-          titleText[1].style.display = 'block';
-          selectBox[2].style.display = 'inline-block';
-          selectBox[3].style.display = 'inline-block';
-        }
-      });
-    };
+   let result = 0;
 
-    changeCell();
+   constructor.addEventListener('change', () => {
 
-    const validDistance = () => {
-      let distance = document.querySelector('.panel-four input');
+     if (checkbox[0].checked) {
+       titleText[1].style.display = 'none';
+       selectBox[2].style.display = 'none';
+       selectBox[3].style.display = 'none';
+       result = 10000;
+     } else {
+       titleText[1].style.display = 'block';
+       selectBox[2].style.display = 'inline-block';
+       selectBox[3].style.display = 'inline-block';
+       result = 15000;
+     }
 
-      distance.addEventListener('input', () => {
-        distance.value = distance.value.replace(/[^\d]/gi, '');
-      });
-    };
+     formControl.forEach((elem) => {
+       if (elem.value == 1) {
+         return;
+       } else {
+         result = result + (result * (+elem.value));
+       }
+     });
 
-    validDistance();
+     if (checkbox[0].checked) {
+       if (checkbox[1].checked) {
+         result += 1000;
+       } else {
+         result;
+       }
+     } else {
+       if (checkbox[1].checked) {
+         result += 2000;
+       } else {
+         result;
+       }
+     }
+
+     calcResult.value = result;
+
+   });
+
+   const validDistance = () => {
+     let distance = document.querySelector('.panel-four input');
+
+     distance.addEventListener('input', () => {
+       distance.value = distance.value.replace(/[^\d]/gi, '');
+     });
+   };
+
+   validDistance();
 
   };
 
--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,38 +1,68 @@
 const calculator = () => {
+const titleText = document.querySelectorAll('.title-text'),
+      selectBox = document.querySelectorAll('.select-box'),
+      constructor = document.querySelector('.constructor'),
+      checkbox = constructor.querySelectorAll('.onoffswitch-checkbox'),
+      calcResult = document.getElementById('calc-result'),
+      formControl = constructor.querySelectorAll('.form-control');
 
-  const myonoffswitch = document.getElementById('myonoffswitch'),
-    titleText = document.querySelectorAll('.title-text'),
-    selectBox = document.querySelectorAll('.select-box');
-
-  const changeCell = () => {
     titleText[1].style.display = 'none';
     selectBox[2].style.display = 'none';
     selectBox[3].style.display = 'none';
+    checkbox[1].checked = false;
 
-    myonoffswitch.addEventListener('change', () => {
-      if (myonoffswitch.checked) {
+    let result = 0;
+
+    constructor.addEventListener('change', () => {
+
+      if (checkbox[0].checked) {
         titleText[1].style.display = 'none';
         selectBox[2].style.display = 'none';
         selectBox[3].style.display = 'none';
+        result = 10000;
       } else {
         titleText[1].style.display = 'block';
         selectBox[2].style.display = 'inline-block';
         selectBox[3].style.display = 'inline-block';
+        result = 15000;
       }
+
+      formControl.forEach((elem) => {
+        if (elem.value == 1) {
+          return;
+        } else {
+          result = result + (result * (+elem.value));
+        }
+      });
+
+      if (checkbox[0].checked) {
+        if (checkbox[1].checked) {
+          result += 1000;
+        } else {
+          result;
+        }
+      } else {
+        if (checkbox[1].checked) {
+          result += 2000;
+        } else {
+          result;
+        }
+      }
+
+      calcResult.value = result;
+
     });
-  };
 
-  changeCell();
+    const validDistance = () => {
+      let distance = document.querySelector('.panel-four input');
 
-  const validDistance = () => {
-    let distance = document.querySelector('.panel-four input');
+        distance.addEventListener('input', () => {
+          distance.value = distance.value.replace(/[^\d]/gi, '');
+        });
+    };
 
-    distance.addEventListener('input', () => {
-      distance.value = distance.value.replace(/[^\d]/gi, '');
-    });
-  };
+    validDistance();
 
-  validDistance();
 
 };
 
